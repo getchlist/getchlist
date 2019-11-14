6d0f25f3fda87a355bff90d3e197e075a1097956
Matei Adriel <rafaeladriel11@gmail.com>
2019-11-14 18:38:57 +0200

    feat(web): isomorphic synchronised localstorage store

---
+++ packages/web/src/common/state/classes/isomorphicStorage.ts
@@ -0,0 +1,50 @@
+import { IS_SERVER } from "./../../../modules/core/constants"
+import { observable, action } from "mobx"
+
+export class IsomorphicStorage<T extends Record<string, unknown>> {
+    @observable
+    public static centralisedStorage = {}
+
+    public get data() {
+        return IsomorphicStorage.centralisedStorage as T
+    }
+
+    public set data(value: T) {
+        Object.assign(IsomorphicStorage.centralisedStorage, value)
+    }
+
+    public constructor(private defaults: T) {
+        if (IS_SERVER) {
+            this.data = defaults
+        } else {
+            for (const key of Object.keys(defaults)) {
+                this.set(key, this.get(key))
+            }
+        }
+    }
+
+    public get(key: keyof T): T[typeof key] {
+        if (IS_SERVER) {
+            return this.data[key] ?? this.defaults[key]
+        } else {
+            const stringifiedKey = String(key)
+            const currentValue = localStorage.getItem(stringifiedKey)
+
+            return currentValue ? JSON.parse(currentValue) : this.defaults[key]
+        }
+    }
+
+    @action
+    public set(key: keyof T, value: T[typeof key]) {
+        if (!IS_SERVER) {
+            localStorage.setItem(String(key), JSON.stringify(value))
+        }
+
+        this.data[key] = value
+
+        return this
+    }
+}
+
+// @ts-ignore
+window.IsomorphicStorage = IsomorphicStorage
--- packages/web/src/modules/auth/stores/authStore.ts
+++ packages/web/src/modules/auth/stores/authStore.ts
@@ -1,10 +1,21 @@
-import { observable, action } from "mobx"
+import { IsomorphicStorage } from "./../../../common/state/classes/isomorphicStorage"
+import { action, computed } from "mobx"
 import { User } from "@getchlist/shared/modules/auth/types/User"
 import { Store } from "../../../common/state/types/Store"
 
 export class AuthStore implements Store {
-    @observable
-    public user: User | null = null
+    private storage = new IsomorphicStorage<{ user: User | null }>({
+        user: null
+    })
+
+    @computed
+    public get user() {
+        return this.storage.data.user
+    }
+
+    public set user(value) {
+        this.storage.set("user", value)
+    }
 
     @action
     public reset() {
